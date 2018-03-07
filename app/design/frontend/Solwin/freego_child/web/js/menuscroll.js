1a902200c73090f87a2abcc0657228714ceca11a
fabioyaxa <fabio.baron@yaxa>
2018-03-06 19:22:07 -0500

    el pro de fabio subio el repo

---
+++ app/design/frontend/Solwin/freego_child/web/js/menuscroll.js
@@ -0,0 +1,11 @@
+$("document").ready(function($){
+    var nav = $('.nav-sections');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 125) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+});
